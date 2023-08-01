1ba0be5ce10145a98db498150e8c1bcfabf6d4d1
Jakub <jakub.kolacz.dev@gmail.com>
2023-08-01 22:31:32 +0200

    Initial commit

---
+++ src/main/java/com/jakus/cheappy/CheappyApplication.java
@@ -0,0 +1,13 @@
+package com.jakus.cheappy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheappyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheappyApplication.class, args);
+	}
+
+}
