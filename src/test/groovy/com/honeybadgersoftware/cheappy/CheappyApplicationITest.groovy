231cf05d0ee7b938c56b36eed161b0733c516ec6
Jakub <jakub.kolacz.dev@gmail.com>
2023-08-03 15:06:03 +0200

    Setup base

---
+++ src/test/groovy/com/honeybadgersoftware/cheappy/CheappyApplicationITest.groovy
@@ -0,0 +1,8 @@
+package com.honeybadgersoftware.cheappy
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class CheappyApplicationITest extends Specification{
+}
