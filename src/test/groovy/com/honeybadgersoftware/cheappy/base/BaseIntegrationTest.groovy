40cb061cf657b79ae328122e2af7242011cae02f
Jakub <jakub.kolacz.dev@gmail.com>
2023-08-08 01:54:19 +0200

    Tests

--- src/test/groovy/com/honeybadgersoftware/cheappy/base/BaseIntegrationTest.groovy
+++ src/test/groovy/com/honeybadgersoftware/cheappy/base/BaseIntegrationTest.groovy
@@ -1,17 +1,50 @@
 package com.honeybadgersoftware.cheappy.base
 
+import jakarta.annotation.PostConstruct
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
-import org.testcontainers.junit.jupiter.Testcontainers
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.web.client.RestTemplate
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.spock.Testcontainers
+import spock.lang.Shared
 import spock.lang.Specification
 
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
-@SpringBootTest
-class BaseIntegrationTest extends Specification{
+@TestPropertySource(locations = "classpath:application-test.properties")
 
-    @Container
+abstract class BaseIntegrationTest extends Specification {
+
+    @LocalServerPort
+    protected int port
+
+    @Shared
+    protected String addressToUseForTests
+
+    @Autowired
+    protected TestRestTemplate restTemplate
+
     static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:15.3")
-            .withDatabaseName("testDb")
+            .withDatabaseName("postgres")
             .withUsername("test")
-            .withPassword("test");
+            .withPassword("test")
 
+    def setup() {
+//        restTemplate = new RestTemplate()
+        addressToUseForTests = "http://localhost:${port}"
+        postgreSQLContainer.start()
+    }
+
+    def cleanup(){
+        postgreSQLContainer.stop()
+    }
 }
--- src/test/groovy/com/honeybadgersoftware/cheappy/controller/UserControllerITest.groovy
+++ src/test/groovy/com/honeybadgersoftware/cheappy/controller/UserControllerITest.groovy
@@ -1,6 +1,53 @@
 package com.honeybadgersoftware.cheappy.controller
 
 import com.honeybadgersoftware.cheappy.base.BaseIntegrationTest
+import com.honeybadgersoftware.cheappy.model.dto.AddressDto
+import com.honeybadgersoftware.cheappy.model.dto.UserDto
+import com.honeybadgersoftware.cheappy.repository.entity.UserEntity
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 
-class UserControllerITest extends BaseIntegrationTest{
+class UserControllerITest extends BaseIntegrationTest {
+
+
+
+    def "should create user and return 201 CREATED"() {
+        given: "a user and address DTO"
+
+        def address = new AddressDto(
+                city: "Warsaw",
+                zipCode: "00-001",
+                street: "Main St",
+                buildingNumber: "123",
+                flatNumber: "45A"
+        )
+
+        def userDto = new UserDto(
+                id: 1L,
+                firstName: "John",
+                lastName: "Doe",
+                email: "john.doe@example.com",
+                password: "password123",
+                address: address  // Dodaj odpowiednie pola dla AddressDto
+        )
+
+        when: "create user endpoint is called"
+        ResponseEntity response = restTemplate.exchange(
+                addressToUseForTests + "/api/users",
+                HttpMethod.POST,
+                new HttpEntity<UserDto>(userDto),
+                ResponseEntity<UserEntity>)
+
+        then: "response is 201 CREATED"
+        response.statusCode == HttpStatus.CREATED
+        response.body.firstName == "John"
+        // Dodaj więcej asercji dla innych pól, jeśli to konieczne
+
+        and: "user is saved in database"
+        // Tutaj możesz sprawdzić, czy użytkownik został zapisany w bazie danych, jeśli masz dostęp do repozytorium
+    }
 }
