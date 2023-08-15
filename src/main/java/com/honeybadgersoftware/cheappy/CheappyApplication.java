298f68cbee2f6e2a0259c064b09517993905705f
Jakub <jakub.kolacz.dev@gmail.com>
2023-08-15 15:28:45 +0200

    cleanup

--- src/main/java/com/honeybadgersoftware/cheappy/CheappyApplication.java
+++ src/main/java/com/honeybadgersoftware/cheappy/CheappyApplication.java
@@ -8,8 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 public class CheappyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CheappyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CheappyApplication.class, args);
+    }
 
 }
